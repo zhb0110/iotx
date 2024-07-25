94802a23ba398fb02f662a45227b26244de4a0c5
zhuhongbo <1010724827@qq.com>
2024-07-25 16:27:13 +0800

    【新增】新增接口用于处理控制令

--- src/main/java/com/example/iotx/controller/DeviceController.java
+++ src/main/java/com/example/iotx/controller/DeviceController.java
@@ -1,10 +1,9 @@
 package com.example.iotx.controller;
 
+import com.example.iotx.entity.RpcObject;
 import com.example.iotx.mqttConfig.MqttGateway;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/device")
@@ -33,9 +32,22 @@ public class DeviceController {
     // 设备反馈：
     //      服务端订阅：topic/devices/rpc/reply/+
     //      设备publish：topic/devices/rpc/reply/deviceName
-    @RequestMapping(value = "rpc", method = {RequestMethod.GET, RequestMethod.POST})
-    public void mqttRpc() {
-        mqttGateway.sendToMqtt("topic/devices/rpc/deviceName", "设备下发数据");
+    @RequestMapping(value = "rpc_get", method = {RequestMethod.GET})
+    public void mqttRpc(@RequestParam("controlValue") String controlValue) {
+        mqttGateway.sendToMqtt("topic/devices/rpc/deviceName", "设备下发数据：" + controlValue);
+        // 监听到设备的反馈则说明消息下发成功，但是经常不需要拿反馈，因为设备的数据会实时上报，这样就能直接看到效果
+    }
+
+    // 命令下发(只有订阅才能用通配符+或#)
+    // 设备下发：
+    //      设备订阅：topic/devices/rpc/+
+    //      服务端publish：topic/devices/rpc/deviceName
+    // 设备反馈：
+    //      服务端订阅：topic/devices/rpc/reply/+
+    //      设备publish：topic/devices/rpc/reply/deviceName
+    @RequestMapping(value = "rpc_post", method = {RequestMethod.POST})
+    public void mqttRpc(@RequestBody RpcObject rpcObject) {
+        mqttGateway.sendToMqtt("topic/devices/rpc/deviceName", "设备下发数据：" + rpcObject.getControlValue());
         // 监听到设备的反馈则说明消息下发成功，但是经常不需要拿反馈，因为设备的数据会实时上报，这样就能直接看到效果
     }
 
---
+++ src/main/java/com/example/iotx/entity/RpcObject.java
@@ -0,0 +1,16 @@
+package com.example.iotx.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @Author Zhb
+ * @create 2024/7/25 下午4:10
+ */
+@Getter
+@Setter
+public class RpcObject {
+
+
+    private String controlValue;
+}
